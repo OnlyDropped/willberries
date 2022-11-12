5a13e6e782c243d8ce58a32a98b7519af55e98f5
OnlyDropped <asheburenkov@yandex.ru>
2022-11-12 17:43:12 +0300

    добавил вывод значения из поля inpit в консоль

--- js/search.js
+++ js/search.js
@@ -1,8 +1,12 @@
 const search = function () {
   const input = document.querySelector('.search-block > input');
-  const searchBtn = document.querySelector('.search-block > button')
-  input.addEventListener('input', (event) => {
-    console.log(event.target.value);
+  // const searchBtn = document.querySelector('.search-block > button')
+  const searchBtn = document.querySelector('.btn-outline-secondary')
+  // input.addEventListener('input', (event) => {
+  //   console.log(event.target.value);
+  // })
+  searchBtn.addEventListener('click', () => {
+    console.log(input.value);
   })
 }
 
